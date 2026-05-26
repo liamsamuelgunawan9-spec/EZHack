5d992b20507bda9692114bd853fbc29793d481d8
LiamS99 <liam.samuel.gunawan9@gmail.com>
2026-05-26 12:08:03 +0000

    fix 35

--- Main.py
+++ Main.py
@@ -226,15 +226,15 @@ blockly_html_payload = f"""
     // Drag Window System Logic
     dragElement(document.getElementById("floatingResultsTab"));
 
-    function dragElement(elmnt) {
+    function dragElement(elmnt) {{
       var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-      if (document.getElementById(elmnt.id + "Header")) {
+      if (document.getElementById(elmnt.id + "Header")) {{
         document.getElementById(elmnt.id + "Header").onmousedown = dragMouseDown;
-      } else {
+      }} else {{
         elmnt.onmousedown = dragMouseDown;
-      }
+      }}
 
-      function dragMouseDown(e) {
+      function dragMouseDown(e) {{
         e = e || window.event;
         // Verify we aren't clicking inside the scrolling console text area itself
         if(e.target.className === "terminalContent") return;
@@ -243,9 +243,9 @@ blockly_html_payload = f"""
         pos4 = e.clientY;
         document.onmouseup = closeDragElement;
         document.onmousemove = elementDrag;
-      }
+      }}
 
-      function elementDrag(e) {
+      function elementDrag(e) {{
         e = e || window.event;
         e.preventDefault();
         pos1 = pos3 - e.clientX;
@@ -254,13 +254,13 @@ blockly_html_payload = f"""
         pos4 = e.clientY;
         elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
         elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
-      }
+      }}
 
-      function closeDragElement() {
+      function closeDragElement() {{
         document.onmouseup = null;
         document.onmousemove = null;
-      }
-    }
+      }}
+    }}
 
     // --- Custom System Blocks ---
     Blockly.Blocks['when_sequence_activated'] = {{
