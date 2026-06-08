78739710f88731d58b9de021b8dccaaa3c223506
LiamS99 <liam.samuel.gunawan9@gmail.com>
2026-06-08 10:11:13 +0000

    massive update

--- assets/action_scan.js
+++
@@ -1,8 +0,0 @@
-python.pythonGenerator.forBlock['action_scan'] = function(block, generator) {
-  var dropdown_scantype = block.getFieldValue('SCANTYPE');
-  var value_name = generator.valueToCode(block, 'NAME', python.Order.ATOMIC) || "''";
-  
-  // Compiles down to an assignment executing our core Python backend utility function
-  var code = 'current_result = run_utility_scan(' + value_name + ', "' + dropdown_scantype + '")\n';
-  return code;
-};
--- assets/display_result.js
+++
@@ -1,5 +0,0 @@
-python.pythonGenerator.forBlock['display_result'] = function(block, generator) {
-  // Directly passes the active result tracker down to our Streamlit view engine
-  var code = 'show_output_to_user(current_result)\n';
-  return code;
-};
